d87bf0d0d0449e6ac1d1b3c4ba900e5ffa4719e4
tea <158533689@qq.com>
2020-03-10 09:21:42 +0800

    '增加了新增，更新，删除以及用户接口'

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,12 +1,48 @@
 //存放blog路由相关内容
 function handleBlogRouter(req,res){
     const method = req.method;
-    if(method=='GET'&&req.path=='/api/blog/list'){
-        //向数据库获取博客信息 
+    if(method == 'GET'&&req.path == '/api/blog/list'){
+        //向数据库获取博客信息
         return{
-            id:'111111' 
+            id:'111111',
+            a:'bbbb'
         }
     }
+
+    if(method=='GET' && req.path=='/api/blog/detail'){
+        // 向数据库获取博客信息
+        return {
+            id:'1',
+            title:'标题1',
+            content:'详情内容XXXXXXXXXXXXX',
+            time:'2353425345'
+        }
+       }
+
+    //新增一篇博客
+    if(method=='POST' && req.path=='/api/blog/new'){
+        // 向数据库获取博客信息
+        return {
+            title:'新增一篇博客'
+        }
+       }
+
+       //更新一篇博客
+    if(method=='POST' && req.path=='/api/blog/update'){
+        // 向数据库获取博客信息
+        return {
+            title:'更新一篇博客'
+        }
+       }
+    
+
+    //删除一篇博客
+    if(method=='POST' && req.path=='/api/blog/del'){
+        // 向数据库获取博客信息
+        return {
+            title:'删除一篇博客'
+        }
+       }
 }
 
-module.exports={handleBlogRouter};
+module.exports=handleBlogRouter;
